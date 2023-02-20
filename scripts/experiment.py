738c84dae916cd9295ee7744eeb5a7cb58eb19c1
zawie <zawie@rice.edu>
2023-02-20 10:34:34 -0600

    better print statements

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -57,9 +57,10 @@ if (args.sample == None):
     raise Exception("kraken execution not implement")
     # (sample_file, weights_file) = run_diversity_sampling(fastq_file)
 else:
-    vprint("Diversity sampling does not need to be ran.")
     sample_path = args.sample 
     weights_path = args.weights
+vprint("Sample file can be located at " + sample_path)
+vprint("Weights file can be located at " + weights_path)
 
 if (args.kraken == None):
     vprint("Kraken needs to be ran")
@@ -67,8 +68,8 @@ if (args.kraken == None):
     raise Exception("kraken execution not implemented")
     # kraken_file = run_kraken(fastq_file)
 else:
-    vprint("Kraken file can be located at " + args.kraken)
     kraken_path = args.kraken
+vprint("Kraken file can be located at " + kraken_path)
 
 # Extract ids from samples 
 ids_list = list()
@@ -85,6 +86,9 @@ with open(weights_path) as infile:
         weight = float(line)
         total_weight += weight
         weights_list.append(weight)
+
+print("Number of weights:", len(weights_list))
+print("Number of IDs:", len(ids_list))
 
 assert(len(weights_list) == len(ids_list))
 
